2d861247bfb870384062cc5f99305975608bdaa1
Ljay Cabel - UCM <cabelljay3211@gmail.com>
2025-09-03 23:35:53 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+/** Demonstration of Java comments */
+public class Comments {
+
+    // main method starts the program
+    public static void main(String[] args) {
+        
+        /* Print a greeting message */
+        System.out.println("Welcome!");
+
+        // Another message
+        System.out.println("Learning comments");
+
+        /** Final message */
+        System.out.println("Done!");
+    }
+}
