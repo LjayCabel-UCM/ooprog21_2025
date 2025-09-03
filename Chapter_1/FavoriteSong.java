714cb3db8d3cbf58554b76b5f960ac278dfb6f4e
Ljay Cabel - UCM <cabelljay3211@gmail.com>
2025-09-03 23:33:01 +0800

    Create  FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,13 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("DIE FOR YOU [The Weeknd]");
+        System.out.println("Even though we're goin' through it");
+        System.out.println("And it makes you feel alone");
+        System.out.println("Just know that I would die for you");
+        System.out.println("Baby, I would die for you, yeah");
+        System.out.println("The distance and the time between us");
+        System.out.println("It'll never change my mind 'cause");
+        System.out.println("Baby, I would die for you, uh");
+        System.out.println("Baby, I would die for you, yeah");
+    }
+}
