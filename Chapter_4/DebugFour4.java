1f2c3d0ab6381c35e6b23f353b1c175af860132c
Ljay Cabel - UCM <cabelljay3211@gmail.com>
2025-09-29 08:45:02 +0800

    Add files via upload

---
+++ Chapter_4/DebugFour4.java
@@ -0,0 +1,39 @@
+// This class uses a DebugTrip class
+// to instantiate three Trip objects
+// Each uses a different version of the constructor
+import java.util.Scanner;
+
+public class DebugFour4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String destinationCity;
+        String departureCity;
+        String mode;
+
+        System.out.print("Enter destination city >> ");
+        destinationCity = input.nextLine();
+
+        System.out.print("Enter departure city >> ");
+        departureCity = input.nextLine();
+
+        System.out.print("Enter mode of transportation >> ");
+        mode = input.nextLine();
+
+        DebugTrip trip1 = new DebugTrip(destinationCity);
+        DebugTrip trip2 = new DebugTrip(destinationCity, departureCity);
+        DebugTrip trip3 = new DebugTrip(destinationCity, departureCity, mode);
+
+        System.out.println("\nTrip 1:");
+        display(trip1);
+        System.out.println("\nTrip 2:");
+        display(trip2);
+        System.out.println("\nTrip 3:");
+        display(trip3);
+    }
+
+    public static void display(DebugTrip trip) {
+        System.out.println("Going to " + trip.getDestination());
+        System.out.println("Leaving from " + trip.getDepartureCity());
+        System.out.println("Going by " + trip.getMode());
+    }
+}
