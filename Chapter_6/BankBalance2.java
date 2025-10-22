3873ce5d235d431261a0e2d5f2123347b9f9531b
Ljay Cabel - UCM <cabelljay3211@gmail.com>
2025-10-22 18:29:07 +0800

    Update and rename BankBalanceCalculator.java to BankBalance2.java

--- Chapter_6/BankBalance2.java
+++ Chapter_6/BankBalance2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BankBalanceCalculator {
+public class BankBalance2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
